deaea2a8bc432eb900db58e4c16abebfbcc91650
ron4a <amsalem.ron@msmail.ariel.ac.il>
2025-02-12 02:11:46 +0200

    Initial commit

---
+++ client.py
@@ -0,0 +1,161 @@
+import socket
+import api
+import argparse
+import api
+
+# region Predefined
+
+# The following are for convenience. You can use them to build expressions.
+pi_c = api.NAMED_CONSTANTS.PI
+tau_c = api.NAMED_CONSTANTS.TAU
+e_c = api.NAMED_CONSTANTS.E
+
+add_b = api.BINARY_OPERATORS.ADD
+sub_b = api.BINARY_OPERATORS.SUB
+mul_b = api.BINARY_OPERATORS.MUL
+div_b = api.BINARY_OPERATORS.DIV
+mod_b = api.BINARY_OPERATORS.MOD
+pow_b = api.BINARY_OPERATORS.POW
+
+neg_u = api.UNARY_OPERATORS.NEG
+pos_u = api.UNARY_OPERATORS.POS
+
+sin_f = api.FUNCTIONS.SIN
+cos_f = api.FUNCTIONS.COS
+tan_f = api.FUNCTIONS.TAN
+sqrt_f = api.FUNCTIONS.SQRT
+log_f = api.FUNCTIONS.LOG
+max_f = api.FUNCTIONS.MAX
+min_f = api.FUNCTIONS.MIN
+pow_f = api.FUNCTIONS.POW
+rand_f = api.FUNCTIONS.RAND
+
+# endregion
+
+
+def process_response(response: api.CalculatorHeader) -> None:
+    if response.is_request:
+        raise api.CalculatorClientError("Got a request instead of a response")
+    if response.status_code == api.CalculatorHeader.STATUS_OK:
+        result, steps = api.data_to_result(response)
+        print("Result:", result)
+        if steps:
+            print("Steps:")
+            expr, first, *rest = steps
+            print(f"{expr} = {first}", end="\n"*(not bool(rest)))
+            if rest:
+                print(
+                    "".join(map(lambda v: f"\n{' ' * len(expr)} = {v}", rest)))
+    elif response.status_code == api.CalculatorHeader.STATUS_CLIENT_ERROR:
+        err = api.data_to_error(response)
+        raise api.CalculatorClientError(err)
+    elif response.status_code == api.CalculatorHeader.STATUS_SERVER_ERROR:
+        err = api.data_to_error(response)
+        raise api.CalculatorServerError(err)
+    else:
+        raise api.CalculatorClientError(
+            f"Unknown status code: {response.status_code}")
+
+
+def client(server_address: tuple[str, int], expression: api.Expression, show_steps: bool = False, cache_result: bool = False, cache_control: int = api.CalculatorHeader.MAX_CACHE_CONTROL) -> None:
+    server_prefix = f"{{{server_address[0]}:{server_address[1]}}}"
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect(server_address)
+        print(f"{server_prefix} Connection established")
+
+        try:
+            request = api.CalculatorHeader.from_expression(
+                expression, show_steps, cache_result, cache_control)
+
+            request = request.pack()
+            print(f"{server_prefix} Sending request of length {len(request)} bytes")
+            client_socket.sendall(request)
+
+            response = client_socket.recv(api.BUFFER_SIZE)
+            print(f"{server_prefix} Got response of length {len(response)} bytes")
+            response = api.CalculatorHeader.unpack(response)
+            process_response(response)
+
+        except api.CalculatorError as e:
+            print(f"{server_prefix} Got error: {str(e)}")
+        except Exception as e:
+            print(f"{server_prefix} Unexpected error: {str(e)}")
+    print(f"{server_prefix} Connection closed")
+
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser(description="A Calculator Client.")
+
+    arg_parser.add_argument("-p", "--port", type=int,
+                            default=api.DEFAULT_SERVER_PORT, help="The port to connect to.")
+    arg_parser.add_argument("-H", "--host", type=str,
+                            default=api.DEFAULT_SERVER_HOST, help="The host to connect to.")
+
+    args = arg_parser.parse_args()
+
+    host = args.host
+    port = args.port
+    """
+
+    The code we added allows users to evaluate mathematical expressions by choosing from predefined options or entering custom input.
+    It communicates with a server using the 'client' function.
+
+    1. Define the predefined expressions (options 1-6).
+    2. The user will enter his choice
+    3. Option to include step by step results and responses from the server or the proxy.
+    4. The user will enter "0" in order to exit the program, that is, to disconnect from the server or the proxy.
+
+    """
+
+    print("Choose an option to choose a calculation:")
+    print("1. max(2, 3) + 3")
+    print("2. 3 + ((4 * 2) / ((1 - 5) * (2 * 3)))")
+    print("3. ((1 + 2) ** (3 * 4)) / (5 * 6)")
+    print("4. -(-((1 + (2 + 3)) ** -(4 + 5)))")
+    print("5. max(2, (3 * 4), log(e), (6 * 7), (9 / 8))")
+    print("6. 4 * (3 + 5)")
+    print("0. Disconnect from the proxy/server")
+    user_choice = input("Enter choice or expression: ").strip()
+
+    while user_choice != "0":
+        try:
+            if user_choice.isdigit() and int(user_choice) in range(1, 6):
+                # Use predefined examples based on user input
+                predefined_expressions = [
+                    mul_b(
+                        div_b(sin_f(max_f(2, mul_b(3, 4), 5, mul_b(6, div_b(mul_b(7, 8), 9)), div_b(10, 11))), 12),
+                        13),
+                    add_b(max_f(2, 3), 3),
+                    add_b(3, div_b(mul_b(4, 2), pow_b(sub_b(1, 5), pow_b(2, 3)))),
+                    div_b(pow_b(add_b(1, 2), mul_b(3, 4)), mul_b(5, 6)),
+                    neg_u(neg_u(pow_b(add_b(1, add_b(2, 3)), neg_u(add_b(4, 5))))),
+                    max_f(2, mul_b(3, 4), log_f(e_c), mul_b(6, 7), div_b(9, 8))
+                ]
+                expr = predefined_expressions[int(user_choice) - 1]
+
+            show_steps = True  # Request the steps of the calculation
+            cache_result = True  # Request to cache the result of the calculation
+
+            # If the result is cached, this is the maximum age of the cached response
+            # that the client is willing to accept (in seconds)
+            cache_control = 2 ** 16 - 1
+
+            # * Change in start (2)
+            client((host, port), expr, show_steps, cache_result, cache_control)
+
+            user_choice = input("Enter choice or expression: ").strip()
+
+        except Exception as e:
+            print(f"Error: Invalid expression input - {e}")
+            exit(1)
+            # Change the following values according to your needs:
+            show_steps = True  # Request the steps of the calculation
+            cache_result = True  # Request to cache the result of the calculation
+            # If the result is cached, this is the maximum age of the cached response
+            # that the client is willing to accept (in seconds)
+            cache_control = 2 ** 16 - 1
+            # * Change in start (2)
+            client((host, port), expr, show_steps,
+                   cache_result, cache_control)
+            user_choice = input("Enter choice or expression: ").strip()
+            # * Change in end (2)
